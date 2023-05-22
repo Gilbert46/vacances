e6ee243f1b03c4bf5117d26a7e0007e1ee284986
Gilbert46 <67496732+Gilbert46@users.noreply.github.com>
2023-05-22 14:50:32 +0200

    Add files via upload

--- www/app.js
+++ www/app.js
@@ -7,16 +7,16 @@ let pais = document.querySelector("#pais");
 let descri = document.querySelector("#descr");
 let cadena = "";
 let container = "";
-let n = 0;
 let vacances = [];
 function AddVacanca() {
-    document.querySelector("#span").innerHTML = "<style>#nom{background-color:#68b850;} #preu{background-color:#68b850;} #hotel{background-color:#68b850;} #pais{background-color:#68b850;}</style>";
+    document.querySelector("#span").innerHTML = "<style>#nom{background-color:#68b850;}#preu{background-color:#68b850;}#hotel{background-color:#68b850;}#pais{background-color:#68b850;}</style>";
     cadena = "!!! falten els següent camps per omplir:";
     if (nom.value == "" || (nom.value.charAt(0) <= '9' && nom.value.charAt(0) >= '0'))
         cadena += " nom," + "<style>#nom{background-color:#fa9393;}</style>";
     if (hotel.value == "" || (hotel.value.charAt(0) <= '9' && hotel.value.charAt(0) >= '0'))
         cadena += " hotel," + "<style>#hotel{background-color:#fa9393;}</style>";
-    if (preu.value == "" || typeof (preu.valueAsNumber) == "string")
+    console.log(preu.value);
+    if (preu.value == "" || preu.valueAsNumber < 0 || preu.valueAsNumber == null)
         cadena += " preu," + "<style>#preu{background-color:#fa9393;}</style>";
     if (pais.value == "Tria un pais de la llista ...")
         cadena += " pais." + "<style>#pais{background-color:#fa9393;}</style>";
